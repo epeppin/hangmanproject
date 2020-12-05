f97944da1e59bb433728e780323f02edc8a69527
epeppin <epeppin@my.wctc.edu>
2020-12-05 14:27:15 -0600

    Final Commit + Comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(
     function () {
 
+        //set available words
         var words = ["JAVASCRIPT", "FUNCTION", "DOCUMENT", "JQUERY", "VARIABLE", "WARNING", "HANDLER", "PROGRAM", "LOGICAL", "DOCUMENT",
             "PROJECT", "HYPERTEXT", "SUBSCRIPT", "STYLESHEET", "LIBRARY"];
         var selectedIndex = "";
@@ -9,9 +10,11 @@ $(document).ready(
         var dashesArray = [];
         newGame();
 
+        //event handlers for letter select and new game
         $("button.letter").click(letterSelect)
         $("button.newGame").click(newGame)
 
+        //start new game
         function newGame() {
             selectedWord = "";
             dashesArray = [];
@@ -30,6 +33,7 @@ $(document).ready(
             selectedIndex = Math.floor(Math.random() * words.length)
             selectedWord = words[selectedIndex]
             splitWord = selectedWord.split("")
+            //loop through word and create dash var to display
             for (var i = 0; i < splitWord.length; i++) {
                 dashesArray.push("-")
                 var dashes = dashesArray.join("")
@@ -38,13 +42,16 @@ $(document).ready(
         }
 
 
-
+        //track number of wrong guesses
         var wrongGuesses = 0;
 
         function letterSelect() {
+            //grey out letter after click
             $(this).addClass("disabled")
             var matchFound = false;
+            //get selected letter
             var letterSelected = $(this).data("letter")
+            //loop through word to find any correct letters
             for (var a = 0; a <= splitWord.length; a++) {
                 if (letterSelected === splitWord[a]) {
                     dashesArray[a] = letterSelected
@@ -53,6 +60,7 @@ $(document).ready(
                 }
                 $("#word").text(dashesArray.join(""))
             }
+            //check how many wrong guesses
             if (matchFound === false) {
                 wrongGuesses++
                 switch (wrongGuesses) {
@@ -84,6 +92,7 @@ $(document).ready(
                         $(".newGame").show();
                         break;
                 }
+                //if all letters are guessed show winning message and hide letter buttons
             } else if (matchFound === true) {
                 if (dashesArray.join("") === splitWord.join("")) {
                     $("#win").show();
